c81e02ae7723ea05d0e1de5640f1158cfb740077
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2023-12-28 10:16:05 +0000

    By yanniboi: Added temporary background music.

--- Scripts/Scenes/main.gd
+++ Scripts/Scenes/main.gd
@@ -2,15 +2,17 @@ extends Node
 
 @onready var menu = $Menu
 @onready var game = $Game
+@onready var music = $Background
 
 func _ready():
 	SignalManager.StartGame.connect(_on_start_game);
 	SignalManager.QuitGame.connect(_on_quit_game);
+	SoundManager.play_sound(music, SoundManager.SOUND_IN_GAME);
 
-func _on_start_game() -> void : 
+func _on_start_game() -> void :
 	menu.visible = false;
 	game.visible = true;
 
-func _on_quit_game() -> void : 
+func _on_quit_game() -> void :
 	menu.visible = true;
 	game.visible = false;
